ad5e1de2462bb3a27e76427042fbd19da592f1f8
VectorASD <60642904+VectorASD@users.noreply.github.com>
2023-03-03 22:25:06 +0700

    Чёт не сварилось

---
+++ Eventinator/Models/CityEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventinator.Models {
+    public class CityEvent {
+        public CityEvent(string lol) {
+            Yeah = lol;
+        }
+        public string Yeah { get; }
+    }
+}
--- Eventinator/ViewModels/MainWindowViewModel.cs
+++ Eventinator/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,18 @@
+using Eventinator.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace Eventinator.ViewModels {
     public class MainWindowViewModel: ViewModelBase {
 
-        public string LOL { get => "\f118"; }
+        public string LOL { get => "\f118\n10"; }
+        private readonly ObservableCollection<CityEvent> eventsList = new() {
+            new CityEvent(),
+            new CityEvent(),
+            new CityEvent()
+        };
+        public ObservableCollection<CityEvent> EventsList { get => eventsList; }
     }
 }
